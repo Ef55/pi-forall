cfc65fc77b56b842be19b9dfae0559785b383c24
Noe De Santo <noe@ef5.ch>
2025-05-06 13:19:49 -0400

    Add compiler benchmarks

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,15 +1,32 @@
 import Criterion.Main
-
-import PiForall.Unbound.Modules qualified as UnPiForall
 import PiForall.AutoEnv.Modules qualified as AutoPiForall
+import PiForall.Unbound.Modules qualified as UnPiForall
 
 -- TODO: suppress IO
+
+prefixes = ["pi/std"]
+
+b name path =
+  bgroup
+    name
+    [ bench "Unbound" $ nfIO (UnPiForall.goFilename prefixes path),
+      bench "AutoEnv" $ nfIO (AutoPiForall.goFilename prefixes path)
+    ]
 
 main =
   defaultMain
     [ bgroup
-        "Lennart"
-        [  bench "Unbound" $ nfIO (UnPiForall.goFilename "pi/examples/Lennart.pi"),
-           bench "AutoEnv" $ nfIO (AutoPiForall.goFilename "pi/examples/Lennart.pi")
+        "/"
+        [ bgroup
+            "Compute"
+            [ b "Lennart" "pi/examples/cLennart.pi",
+              b "Compiler" "pi/examples/cCompiler.pi"
+            ],
+          bgroup
+            "Typechecking"
+            [ b "Compiler" "pi/examples/Compiler.pi"
+            -- TODO: fix bug in unbound version
+              --b "AVL" "pi/examples/AVL.pi"
+            ]
         ]
     ]
